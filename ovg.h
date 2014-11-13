d928fb1a19155c0fc0c2eb8f16f0c0fa386d0ec5
pzl <dan@panzarel.la>
2014-11-12 20:56:24 -0500

    library header group functions

--- ovg.h
+++ ovg.h
@@ -13,10 +13,11 @@ typedef enum {
   JOIN_BEVEL                               = 0x1802
 } JoinStyle;
 
-
-
 #define BG_COLOR { 0.94f, 1.0f, 0.92f, 1.0f }
 
+/*****************************
+* Management Commands
+******************************/
 void ovg_init(void);
 void ovg_wininfo(int *x, int *y, int *w, int *h);
 void ovg_cleanup(void);
@@ -25,6 +26,9 @@ void ovg_clear(void);
 void ovg_clear_rect(int x, int y, int w, int h);
 
 
+/*****************************
+* Fills n Strokes
+*****************************/
 void ovg_fill(unsigned char r, unsigned char g, unsigned char b, unsigned char a);
 void ovg_stroke(unsigned char r, unsigned char g, unsigned char b, unsigned char a);
 
@@ -35,6 +39,10 @@ void ovg_stroke_miter(float limit);
 void ovg_dash(int *pattern, int n);
 void ovg_dash_phase(int phase);
 
+
+/***************************
+* Shapes and Objects
+****************************/
 void ovg_line(int x0,int y0,int x1,int y1);
 void ovg_polyline(int *x, int *y, int n);
 void ovg_polygon(int *x, int *y, int n);
@@ -46,6 +54,10 @@ void ovg_arc(int x, int y, int w, int h, int star_angle, int angle_travel);
 void ovg_text(int x, int y, char *s, int pointsize);
 
 
+
+/************************
+* Transformations
+*************************/
 void ovg_translate(int x, int y);
 void ovg_rotate(int r);
 void ovg_scale(float x, float y);
